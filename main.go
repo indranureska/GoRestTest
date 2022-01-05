36bf02a43ca554fa819acc2d02f188845b02885d
Indra Nureska <indra.nureska@gmail.com>
2022-01-06 06:41:33 +0800

    code update

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"io"
+	"net/http"
 	"time"
 
 	"github.com/emicklei/go-restful"
 )
 
 func main() {
-	// // Create a web service
-	// webservice := new(restful.WebService)
-	// // Create a route and attach it to handler in the service
-	// webservice.Route(webservice.GET("/ping").To(pingTime))
-	// // Add the service to application
-	// restful.Add(webservice)
-	// http.ListenAndServe(":8000", nil)
+	// Create a web service
+	webservice := new(restful.WebService)
+	// Create a route and attach it to handler in the service
+	webservice.Route(webservice.GET("/ping").To(pingTime))
+	// Add the service to application
+	restful.Add(webservice)
+	http.ListenAndServe(":8000", nil)
 
 	// runMySqlDbTest()
-	runMongoDbTest()
+	// runMongoDbTest()
 }
 
 func pingTime(req *restful.Request, resp *restful.Response) {
 	// Write to the response
-	io.WriteString(resp, fmt.Sprintf("%s", time.Now()))
+	io.WriteString(resp, time.Now().String())
 }
